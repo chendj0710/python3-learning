61e77ea513582e94da76f8035ad0006d005c689a
git <chendj_1506@163.com>
2019-06-27 18:38:14 +0800

    1.增加books

---
+++ code/if.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+#判断列表是否为空
+cars = []
+if cars :
+    print('Not null')
+else :
+    print('Null')
+
+cars = []
+if not cars :
+    print('Null')
+else :
+    print('Not null')
---
+++ code/list.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#create names list
+names = ['zhangsan', 'wagner', 'mazi', 'lisi']
+
+
+#treavesing list
+for name in names:
+    print(name + ', that was a great man')
+
+print('Thank you, everybody')
+
+
+#数字列表
+squares = [value**2 for value in range(1, 6) ]
+print(squares)
+
+#print 1-1000000
+nums = list(range(1, 1000001))
+print(nums)
+
+print('nums min:' + str(min(nums)) + ', max:' + str(max(nums)))
+print('sum(nums):' + str(sum(nums)))
+
+#print 1-20间的基数
+for num in range(1, 20, 2):
+    print(num)
+
+#create list 1-10 的立方
+nums = [value**3 for value in range(1, 11)]
+print(nums)
+
+#treavesing 切片list
+for num in nums[:3]:
+    print(num) 
+
+#降序输出nums前三
+for num in sorted(nums, reverse=True)[:3]:
+    print(num)
