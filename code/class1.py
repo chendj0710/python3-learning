339a6d04d8b07694a7f7aa79d013d95722e9776d
git <chendj_1506@163.com>
2019-07-02 18:10:32 +0800

    study class

--- code/class1.py
+++ code/class1.py
@@ -16,8 +16,22 @@ class User():
         print("Hi, " + self.first_name + " " + self.last_name)
         print("")
 
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name, privileges):
+        super().__init__(first_name, last_name)
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print(self.privileges)
+
+
 user1 = User('ergou', 'wang')
 user2 = User('kangkang', 'li')
 
 user1.descriper()
 user1.greet_user()
+
+admin1 = Admin('root', 'zhao', 'can add post')
+admin1.show_privileges()
